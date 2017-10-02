2110308c6b87e548772cca2632706e2112be1597
practice-08 <31637987+practice-08@users.noreply.github.com>
2017-10-02 10:38:11 -0700

    Added HiLow3

--- src/org/teachingkidsprogramming/section03ifs/HiLow3.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow3.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow2
+public class HiLow3
 {
   public static void main(String[] args)
   {
@@ -15,10 +15,16 @@ public class HiLow2
     //int answer = (int) (Math.random() * 100 + 1);
     // System.out.println("The answer is " + answer);
     // int answer = 40;
-    
-    for (int i = 1; i <= attemps; i++)
+    int upperRange = 100;
+    for (int i = 1; i <= 8; i++)
     {
       int guess = MessageBox.askForNumericalInput("What is your guess?");
+      // input validation
+      while (guess < 1 || guess > upperRange)
+      {
+        MessageBox.showMessage("Please enter a valid guess between 1 and " + upperRange);
+        guess = MessageBox.askForNumericalInput("What is your guess?");
+      }
       if (guess == answer)
       {
         Sound.playBeep();
@@ -33,11 +39,11 @@ public class HiLow2
       {
         MessageBox.showMessage("Too low lmao");
       }
-      if (i == attemps)
+      if (i == 8)
       {
         MessageBox.showMessage("You've lost");
       }
-      MessageBox.showMessage("You have " + (attemps - i) + " attemps left.");
+      MessageBox.showMessage("You have " + (8 - i) + " attemps left.");
     }
   }
 }
