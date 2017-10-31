c2136ec4129c8aae7f8a69743a99a9d187488f6e
practice-08 <31637987+practice-08@users.noreply.github.com>
2017-10-31 10:01:17 -0700

    Tkp

--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -10,38 +10,38 @@ public class PentagonCrazy
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //    createColorPalette (recipe below) --#8.1
+    createColorPalette();
     //
-    //    ------------- Recipe for createColorPalette --#8.2
-    //    Add steel blue to the color wheel --#7
-    ColorWheel.addColor(PenColors.Blues.SteelBlue);
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
-    //    ------------- End of createColorPalette recipe --#8.3
     //
-    //    drawPentagon (recipe below) --#10.1
-    //
-    //    ------------- Recipe for drawPentagon --#10.2
+    drawPentagon();
+  }
+  private static void drawPentagon()
+  {
     for (int i = 0; i < 200; i++)
     {
-      //        adjustPen (recipe below) --#9.1
       //
       adjustPen();
       //
-      //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
       int length = i;
       Tortoise.move(length);
       Tortoise.turn(360 / 5);
       Tortoise.turn(1);
     }
-    //    ------------- End of drawPentagon recipe --#10.3
+  }
+  private static void createColorPalette()
+  {
+    //
+    ColorWheel.addColor(PenColors.Blues.SteelBlue);
+    ColorWheel.addColor(PenColors.Purples.Orchid);
+    ColorWheel.addColor(PenColors.Blues.DarkSlateBlue);
+    ColorWheel.addColor(PenColors.Greens.Teal);
+    ColorWheel.addColor(PenColors.Blues.Aquamarine); // wrong, *make indigo*
   }
   private static void adjustPen()
   {
     Tortoise.setPenColor(ColorWheel.getNextColor());
-    //        Increase the tortoises pen width by 1 --#15                                              
+    //        Increase the tortoises pen width by 1 --#15    
+    Tortoise.setPenWidth(+1);
     //        If the tortoise's pen width is greater than 4, then --#17
     //            Reset the pen width to 1 --#16
   }
