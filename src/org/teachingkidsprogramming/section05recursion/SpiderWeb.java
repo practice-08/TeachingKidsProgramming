2ea50e3d95d6f16bf66d13e58ccfc4d938ed709b
practice-08 <31637987+practice-08@users.noreply.github.com>
2017-11-17 13:20:56 -0800

    tkp

--- src/org/teachingkidsprogramming/section05recursion/SpiderWeb.java
+++ src/org/teachingkidsprogramming/section05recursion/SpiderWeb.java
@@ -1,39 +1,45 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#6
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(1);
     //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
+    Tortoise.setPenColor(PenColors.Reds.Red);
     //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
-    //    The current length of a line is 10.5 pixels --#1.2                  
-    //    The current zoom is 1.1 --#8.2
-    //    Do the following 10 times --#10.1
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
-    //        Do the following 6 times --#5.1
-    //            drawTriangle (recipe below) --#4.1
-    //
-    //            ------------- Recipe for drawTriangle --#4.2
-    //            Do the following 3 times --#3.1
-    //                Move the tortoise the current length (of the line) --#1.1
-    //                Turn the tortoise 1/3rd of 360 degrees --#2
-    //            End Repeat --#3.2
-    //            ------------- End of drawTriangle recipe --#4.3
-    //
-    //            Turn the tortoise 1/6th of 360 degrees to the right --#7
-    //            Increase the current length (of the line) by the current zoom --#8.1
-    //        End Repeat --#5.2
-    //        ------------- End of weaveOneLayer recipe --#9.3
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    End Repeat --#10.2
+    double length = 10.5;
+    double zoom = 1.1;
+    for (int i = 0; i < 10; i++)
+    {
+      length = weaveOneLayer(length, zoom);
+      //
+      zoom = zoom * 1.3;
+    }
+  }
+  private static double weaveOneLayer(double length, double zoom)
+  {
+    for (int i = 0; i < 6; i++)
+    {
+      //
+      drawTriangle(length);
+      //
+      Tortoise.turn(360 / 6);
+      length = length + zoom;
+    }
+    return length;
+  }
+  private static void drawTriangle(double length)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      Tortoise.move(length);
+      Tortoise.turn(360 / 3);
+    }
   }
 }
--- src/org/teachingkidsprogramming/section05recursion/TreeQuiz.java
+++ src/org/teachingkidsprogramming/section05recursion/TreeQuiz.java
@@ -9,27 +9,47 @@ public class TreeQuiz extends TreeQuizAdapter
   {
     //   Question 1 
     //   doubleLength (recipe below) 
-    //   ------------- Recipe for doubleLength 
-    //       make the current length twice what it is
-    //   ------------- End of doubleLength recipe
+    doubleLength();
     //
     //   Question 2 
     //   decreaseTurn (recipe below) 
-    //   ------------- Recipe for decreaseTurn 
-    //      decrease the current turn by 1
-    //   ------------- End of decreaseTurn recipe
+    decreaseTurn();
     //
     //   Question 3 
     //   setNinety (recipe below) 
-    //   ------------- Recipe for setNinety 
-    //       set the current turn angle to 90, HINT: Use angles.put(key,value);
-    //   ------------- End of setNinety recipe
+    setNinety();
     //
     //   Question 4 
+    angleFive();
+  }
+  private void angleFive()
+  {
     //   angleFive (recipe below) 
     //   ------------- Recipe for angleFive 
     //       sets the 5th angle to 36 degrees
+    angles.put(5, 36);
     //   ------------- End of angleFive recipe
+  }
+  private void setNinety()
+  {
+    //   ------------- Recipe for setNinety 
+    //       set the current turn angle to 90, HINT: Use angles.put(key,value);
+    angles.put(turn, 90);
+    //   ------------- End of setNinety recipe
+  }
+  private void decreaseTurn()
+  {
+    //   ------------- Recipe for decreaseTurn 
+    //      decrease the current turn by 1
+    turn = turn - 1;
+    //   ------------- End of decreaseTurn recipe
+  }
+  private void doubleLength()
+  {
+    //   ------------- Recipe for doubleLength 
+    //       make the current length twice what it is
+    length = length * 2;
+    //   ------------- End of doubleLength recipe
   }
   public static void main(String[] args)
   {
