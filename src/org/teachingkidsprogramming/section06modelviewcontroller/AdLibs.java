33bd10a97193d73b510979105d53a4b571ed319a
practice-08 <31637987+practice-08@users.noreply.github.com>
2017-11-28 10:00:24 -0800

    deep dive and adlibs

--- src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -7,11 +7,14 @@ public class AdLibs
   public static void main(String[] args)
   {
     //  Ask the user to enter an adverb, save it as currentAdverb --#2
-    String currentAdverb = MessageBox.askForTextInput("Enter an adverb ");
+    String currentAdverb = MessageBox.askForTextInput("Enter an adverb: ");
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
+    String currentEdVerb = MessageBox.askForTextInput("Enter a verb ending in -ed ");
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
+    String currentBodyPart = MessageBox.askForTextInput("Enter a body part: ");
     //  Set the value of the currentStory to the word "Today " --#1.2
-    String currentStory = "Today ";
+    String currentStory = ("Today " + "I woke " + currentAdverb + "." + "Then I " + currentEdVerb + " " + "my "
+        + currentBodyPart + ". ");
     //  Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
     //  Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7
