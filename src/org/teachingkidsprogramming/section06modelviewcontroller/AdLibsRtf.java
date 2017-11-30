a25426e70eeaf1f0f0952efa00228bd42809f0c1
practice-08 <31637987+practice-08@users.noreply.github.com>
2017-11-30 14:30:01 -0800

    Adlibs

--- src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibsRtf.java
+++ src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibsRtf.java
@@ -1,16 +1,27 @@
 package org.teachingkidsprogramming.section06modelviewcontroller;
 
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+import org.teachingextensions.logo.utils.MVCUtils.Parser;
+import org.teachingextensions.logo.utils.MVCUtils.Viewer;
 
 public class AdLibsRtf
 {
+  public static class Words
+  {
+    public String adverb;
+    public String edVerb;
+    public String bodyPart;
+  }
   public static void main(String[] args)
   {
-    //  Create a new 'word' container your story's words (uncomment line 10) --#1.1
-    //  Words word = new Words();
-    //  Ask the user to enter an adverb, save it as currentAdverb --#2
-    //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#3
-    //  Ask the user to enter a body part, save it as currentBodyPart --#4
-    //  Connect the words in the currentStory to an RTF file parser (use the Parser object)  --#1.2
-    //  Display the currentStory in an RTF file (use the Viewer object) --#1.3
+    Words word = new Words();
+    word.adverb = MessageBox.askForTextInput("Enter an adverb: ");
+    word.edVerb = MessageBox.askForTextInput("enter a verb ending in '-ed'");
+    word.bodyPart = MessageBox.askForTextInput("Enter a body part:");
+    Parser.parseRtfFile("view.rtf", word);
+    String currentStory = "Today I woke up and " + word.adverb + ". ";
+    currentStory = currentStory + "Then I " + word.edVerb + " ";
+    currentStory = currentStory + "my " + word.bodyPart + ". ";
+    Viewer.displayRtfFile(currentStory);
   }
 }
